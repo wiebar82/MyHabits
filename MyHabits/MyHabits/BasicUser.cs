8c36eacacce418b5c3fe036b9ee6830e9dd124ec
Bartek Wieczorek <wieczorek.bartlomiej@wp.pl>
2024-03-24 19:36:49 +0100

    utworzenie interfejsu i BasicUser

---
+++ MyHabits/MyHabits/BasicUser.cs
@@ -0,0 +1,18 @@
+﻿namespace MyHabits
+{
+    public abstract class BasicUser : IUser
+    {
+        public BasicUser(string userName, string userLast)
+        {
+            this.UserName = userName;
+            this.UserLast = userLast;
+        }
+
+        public string UserName { get; set; }
+        public string UserLast { get; set; }
+
+        public abstract void AddRating(float rating);
+
+        public abstract void AddRating(string rating);
+    }
+}
---
+++ MyHabits/MyHabits/IUser.cs
@@ -0,0 +1,11 @@
+﻿namespace MyHabits
+{
+    public interface IUser
+    {
+        string UserName { get; }
+        string UserLast { get; }
+
+        void AddRating(float rating);
+        void AddRating(string rating);
+    }
+}
